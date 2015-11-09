12d72000b3871b967ff9c4e6b7298fa825fcb3e1
Bilal Raza <hafizbilal112@gmail.com>
2015-11-10 00:52:53 +0500

    Update config, lib and dependencies.

--- webpack.config.js
+++ webpack.config.js
@@ -22,7 +22,7 @@ module.exports = {
     loaders: [
       {
         test: /\.js$/,
-        loader: "awesome-typescript-loader?doTypeCheck=false&useBabel=true&useWebpackText=true",
+        loader: "awesome-typescript-loader?doTypeCheck=false&useWebpackText=true",
         include: [wwwPath],
         // include: /www(\/|\\)app(\/|\\)/,
         exclude: /node_modules/
@@ -39,14 +39,12 @@ module.exports = {
   resolve: {
     modulesDirectories: [
       "node_modules",
-      "node_modules/ionic-framework/src/es5/common", // ionic-framework npm package (stable)
       "node_modules/ionic-framework/node_modules", // angular is a dependency of ionic
-      "node_modules/ionic-framework/js", // for web-animations polyfill
+      "node_modules/ionic-framework/dist/src/es5/common", // ionic-framework npm package (stable)
+      "node_modules/ionic-framework/dist/js", // for web-animations polyfill
 
-      // "node_modules/ionic2/dist/src/es5/common", // driftyco/ionic2 repo (master)
-      // "dist/src/es5/common",
-      // "node_modules/ionic2/node_modules",
-      // "node_modules/ionic2/dist/js"
+      // See README for steps on developing against ionic-framework locally
+      // "dist/src/es5/common" // when developing against locally linked ionic-framework (master)
     ],
     extensions: ["", ".js", ".ts"]
   }
